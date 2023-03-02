8c93dfe1bbd2f126cfccf07c1b07d2d9dea2eab7
Jaskirat Kaur <jaskiratk@linux3.cs.uchicago.edu>
2023-03-02 01:36:30 -0600

    some changes to app

--- main/src/app.py
+++ main/src/app.py
@@ -1,16 +1,25 @@
 import json
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 import plotly.express as px
-from filter_sentiment import clean_viz, filter_for_visualization
+import pandas as pd
+from dash.dependencies import Input, Output
+import plotly.graph_objects as go
 
 # Load the geojson data
-with open('/sources/us-states.json', 'r') as f:
+with open("/home/jaskiratk/capp30122/30122-project-hot-or-not/main/sources/us-states.json", 'r') as f:
     geojson_data = json.load(f)
 
 # Load sentiment data file
-sentiment_df = filter_for_visualization("sources/US_twitter_analysis_state.csv")
+sentiment_data = pd.read_csv("/home/jaskiratk/capp30122/30122-project-hot-or-not/main/sources/twitter_year_state.csv")
+sentiment_df = sentiment_data[(sentiment_data['year'] != 2006) & (sentiment_data['year'] != 2007)]
+
+# county data for time series
+time_series_data = pd.read_csv("/home/jaskiratk/capp30122/30122-project-hot-or-not/main/sources/twitter_year_county.csv")
+
+# Create the app
+app = dash.Dash(__name__)
 
 # Define a function that takes a year as input and returns a new Choropleth map with the sentiment scores for that year.
 def update_map(year):
@@ -23,63 +32,31 @@ def update_map(year):
 # Create the initial Choropleth map using the sentiment scores for the first year in the data
 fig = update_map(sentiment_df['year'].min())
 
-# Define the list of year options that will be displayed in the dropdown menu.
-year_options = sentiment_df['year'].unique()
+# Define the range of years for the slider
+year_range = [sentiment_df['year'].min(), sentiment_df['year'].max()]
 
-# Create a list of dictionaries that define the behavior of the dropdown menu. Each dictionary corresponds to a dropdown menu button,
-# and contains the label of the button and the behavior when the button is clicked. In this case, we want to call the update_map
-# function with the selected year.
-dropdown_buttons = [
-    {
-        'label': str(year),
-        'method': 'update',
-        'args': [{'z': [sentiment_df[sentiment_df['year'] == year]['sentiment'].tolist()]}]
-    }
-    for year in year_options
-]
-
-# Add the dropdown menu to the Plotly Figure object using the update_layout method.
-fig.update_layout(
-    updatemenus=[
-        {
-            'buttons': dropdown_buttons,
-            'direction': 'down',
-            'pad': {'r': 10, 't': 10},
-            'showactive': True,
-            'type': 'buttons',
-            'x': 0.1,
-            'xanchor': 'left',
-            'y': 1.1,
-            'yanchor': 'top'
-        }
-    ],
-    title={
-        'text': 'Climate Change Sentiment Analysis',
-        'x': 0.5,
-        'y': 0.95,
-        'xanchor': 'center',
-        'yanchor': 'top'
-    }
+# Create the slider
+slider = dcc.RangeSlider(
+    id='year-slider',
+    min=year_range[0],
+    max=year_range[1],
+    value=[year_range[0], year_range[1]],
+    marks={str(year): str(year) for year in sentiment_df['year'].unique()}
 )
 
-# Define the dashboard layout using Dash HTML components
-app = dash.Dash(__name__)
-app.layout = html.Div(children=[
-    html.H1(children='Climate Change Sentiment Analysis'),
-
-    html.Div(children='''
-        A visualization of sentiment analysis data related to climate change in US states.
-    '''),
-
-    dcc.Graph(
-        id='climate-change-sentiment-map',
-        figure=fig
-    ),
-
-    html.Footer(children=[
-        html.P('Authors: Jonathan Juarez, Jaskirat Kaur, Ridhi Purohit, Grey Xu')
-    ])
+# Define the app layout
+app.layout = html.Div([
+    dcc.Graph(id='map', figure=fig),
+    slider
 ])
+
+# Define the app callback
+@app.callback(
+    Output('map', 'figure'),
+    Input('year-slider', 'value')
+)
+def update_map_callback(years):
+    return update_map(years[0])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
