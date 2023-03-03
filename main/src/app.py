9ee722239b69af5f47b954e0b031c5bef014b4e3
Jaskirat Kaur <jaskiratk@linux3.cs.uchicago.edu>
2023-03-02 20:31:30 -0600

    modified app

--- main/src/app.py
+++ main/src/app.py
@@ -6,57 +6,100 @@ import plotly.express as px
 import pandas as pd
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
+from stance_define import stance_data
+import dash_bootstrap_components as dbc
 
-# Load the geojson data
+df = pd.read_csv("/home/jaskiratk/capp30122/30122-project-hot-or-not/main/sources/twitter_compressing_effort.csv")
+
 with open("/home/jaskiratk/capp30122/30122-project-hot-or-not/main/sources/us-states.json", 'r') as f:
-    geojson_data = json.load(f)
+    geojson_file = json.load(f)
 
-# Load sentiment data file
-sentiment_data = pd.read_csv("/home/jaskiratk/capp30122/30122-project-hot-or-not/main/sources/twitter_year_state.csv")
-sentiment_df = sentiment_data[(sentiment_data['year'] != 2006) & (sentiment_data['year'] != 2007)]
 
-# county data for time series
-time_series_data = pd.read_csv("/home/jaskiratk/capp30122/30122-project-hot-or-not/main/sources/twitter_year_county.csv")
+# Add title and authors
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+# app.title = 'Climate Change Sentiment Analysis'
+# authors = "Jonathan Juarez, Jaskirat Kaur, Ridhi Purohit, Grey Xu"
 
-# Create the app
-app = dash.Dash(__name__)
 
-# Define a function that takes a year as input and returns a new Choropleth map with the sentiment scores for that year.
-def update_map(year):
-    fig = px.choropleth_mapbox(sentiment_df[sentiment_df['year'] == year], geojson=geojson_data, color='sentiment',
-                               locations='state_FIP', featureidkey='properties.STATE',
-                               center={'lat': 37.0902, 'lon': -95.7129}, mapbox_style="carto-positron", zoom=3,
-                               color_continuous_scale='rdylgn', range_color=(-0.3, 0.3))
-    return fig
+app.layout = dbc.Container([
+    dbc.Row(
+        dbc.Col(
+            [
+                html.H4("Sentiment of US states", className="mb-0"),
+                html.P(
+                    "† This project aims to analyze the sentiment towards climate change across the United States. We collected data from social media platforms such as Twitter and analyzed the sentiment towards climate change based on the stance of the user (Believer, Denier or Neutral). The map below shows the average sentiment towards climate change by state for the selected year and stance.",
+                    className="text-muted",
+                ),
+            ],
+            width={"size": 8, "offset": 2},
+            className="my-3",
+        )
+    ),
+    dbc.Row(
+        [
+            dbc.Col(
+                [
+                    html.Label(['Choose stance:'],style={'font-weight': 'bold'}),
+                    dcc.Dropdown(
+                        id='stance_dropdown',
+                        options=[
+                            {'label': 'Believer', 'value': 'believer'},
+                            {'label': 'Denier', 'value': 'denier'},
+                            {'label': 'Neutral', 'value': 'neutral'}
+                        ],
+                        value='believer',
+                        style={'width': '100%'}
+                    ),
+                    html.Br(),
+                    dcc.Graph(id='map-graph')
+                ],
+                width={"size": 8},
+                className="my-3",
+            ),
+            dbc.Col(
+                [
+                    html.Label(['Choose year:'],style={'font-weight': 'bold'}),
+                    dcc.Slider(
+                        id='year_slider',
+                        min=2009,
+                        max=2019,
+                        value=2009,
+                        marks={str(year): str(year) for year in range(2009, 2020, 1)},
+                        step=None
+                    ),
+                ],
+                width={"size": 8},
+                className="my-3 align-self-end",
+            ),
+        ]
+    ),
+    dbc.Row(
+        dbc.Col(
+            [
+                html.P(
+                    "† This project was developed by Jonathan Juarez, Jaskirat Kaur, Ridhi Purohit, and Grey Xu as part of the Spring 2023 course 'CAPP 30122: Computer Science and Applications II' at the University of Chicago",
+                    className="text-muted",
+                ),
+            ],
+            width={"size": 10, "offset": 1},
+            className="my-3",
+        )
+    ),
+], fluid=True)
 
-# Create the initial Choropleth map using the sentiment scores for the first year in the data
-fig = update_map(sentiment_df['year'].min())
-
-# Define the range of years for the slider
-year_range = [sentiment_df['year'].min(), sentiment_df['year'].max()]
-
-# Create the slider
-slider = dcc.RangeSlider(
-    id='year-slider',
-    min=year_range[0],
-    max=year_range[1],
-    value=[year_range[0], year_range[1]],
-    marks={str(year): str(year) for year in sentiment_df['year'].unique()}
-)
-
-# Define the app layout
-app.layout = html.Div([
-    dcc.Graph(id='map', figure=fig),
-    slider
-])
-
-# Define the app callback
 @app.callback(
-    Output('map', 'figure'),
-    Input('year-slider', 'value')
+    Output('map-graph', 'figure'),
+    [Input('stance_dropdown', 'value'),
+     Input('year_slider', 'value')]
 )
-def update_map_callback(years):
-    return update_map(years[0])
+def update_map(stance, year):
+    filtered_df = df[(df['stance'] == stance) & (df['year'] == year)]
+    avg_sentiment_df = filtered_df.groupby('state_FIP')['sentiment'].mean().reset_index()
+    fig = px.choropleth_mapbox(avg_sentiment_df, geojson=geojson_file, color='sentiment',
+                                  locations='state_FIP', featureidkey='properties.STATE',
+                                  center={'lat': 37.0902, 'lon': -95.7129}, mapbox_style="carto-positron", zoom=3,
+                                  color_continuous_scale='rdylgn', range_color=(-0.3, 0.3))
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ main/src/stance_define.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+
+US_twitter = pd.read_csv("/home/jaskiratk/capp30122/30122-project-hot-or-not/main/sources/twitter_compressing_effort (1) (1).csv")
+US_twitter['stance'] = US_twitter['stance'].replace([3, 2, 1], ['believer', 'denier', 'neutral'])
+file_path = '/home/jaskiratk/capp30122/30122-project-hot-or-not/main/sources/twitter_compressing_effort.csv'
+US_twitter.to_csv(file_path, index=False)
+
+    
+def stance_data(stance):
+    dataframe = US_twitter = pd.read_csv("/home/jaskiratk/capp30122/30122-project-hot-or-not/main/sources/twitter_compressing_effort.csv")
+    filtered_df = dataframe[dataframe["stance"] == stance]
+    grouped_df = filtered_df.groupby(['year', 'state_FIP'])
+
+    # Define the aggregation functions for categorical and numerical variables
+    # For categorical value, take the most frequently occured type, for the numerical value, take the average
+    agg_dict = {
+                'stance': lambda x: x.value_counts().index[0], 
+                'sentiment': 'mean',}
+
+    # Apply the aggregation
+    result_df = grouped_df.agg(agg_dict)
+
+    # Reset the index
+    stance_df = result_df.reset_index()
+    stance_df = stance_df[(stance_df['year'] != 2006) & (stance_df['year'] != 2007) & (stance_df['year'] != 2008)]
+
+    return stance_df
+
+
+
+
+
+    
