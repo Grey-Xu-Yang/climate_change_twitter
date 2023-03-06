dd718f01aa6fa88240d70f2bcaf1cc4f1d745612
Jaskirat Kaur <jaskiratk@linux3.cs.uchicago.edu>
2023-03-06 16:26:23 -0600

    updated

--- main/src/app.py
+++ main/src/app.py
@@ -8,6 +8,8 @@ Main file for building the dashboard.
 
 Run the app with `python3 app.py` and visit
 http://127.0.0.1:8050/ in your web browser.
+(takes about a minute to run)
+
 '''
 
 import json
@@ -18,36 +20,40 @@ import plotly.express as px
 import pandas as pd
 import base64
 from dash.dependencies import Input, Output
-import plotly.graph_objects as go
-
-from data4dash import add_state_column
+from .utils import add_state_column, extract_year_month
 import dash_bootstrap_components as dbc
 
-believer_df = pd.read_csv("/home/jaskiratk/capp30122/30122-project-hot-or-not/main/sources/believer/believer_twitter.csv")
-denier_df = pd.read_csv("/home/jaskiratk/capp30122/30122-project-hot-or-not/main/sources/denier_twitter.csv")
-neutral_df = pd.read_csv("/home/jaskiratk/capp30122/30122-project-hot-or-not/main/sources/neutral_twitter.csv")
+
+# load dataframes for the three stances
+believer_df = pd.read_csv("./main/sources/believer_twitter.csv")
+denier_df = pd.read_csv("./main/sources/denier_twitter.csv")
+neutral_df = pd.read_csv("./main/sources/neutral_twitter.csv")
 
 final_df = pd.concat([believer_df, denier_df, neutral_df])
 
-with open("/home/jaskiratk/capp30122/30122-project-hot-or-not/main/sources/us-states.json", 'r') as f:
+with open("./main/sources/us-states.json", 'r') as f:
     geojson_file = json.load(f)
 
-# ******************************** Data cleaning ****************************** #
-
+# Adding state column based on state FIP
 df = add_state_column(final_df)
 
-# ------------------------- App Layout ------------------------------- #
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MINTY])
+# ****************** Data cleaning for disaster scatter plot ***************** #
 
-L_FONT = "Work Sans, sans-serif"
-LT_SIZE = 16
-L_SIZE = 12
+df_disaster = pd.read_csv('./main/sources/average_sentiments_data.csv')
+
+# Converting date into year and month
+df2 = extract_year_month(df_disaster)
+
+# --------------------------------- App Layout ------------------------------- #
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MINTY])
 
 app.layout = dbc.Container([
     dbc.Row(
         dbc.Col(
             [
-                html.H1("Sentiment of US states", style = {'textAlign': 'center'}),
+                html.H1("Hot or Not: An Enquiry into Climate Change Perceptions", 
+                        style = {'textAlign': 'center', "padding-top": "70px"}),
                 html.P(
                     "Welcome to our climate sentiment analysis dashboard! Our project is dedicated"
                     " to analyzing the sentiment towards climate change across the United States."
@@ -55,7 +61,7 @@ app.layout = dbc.Container([
                     " towards climate change based on the stance of the user (Believer, Denier or Neutral." 
                     " Explore our dashboard to gain insights into how people feel about this critical issue"
                     " in different regions of the country.",
-                    style={"font-size": 16, "text-align": 'left', 'marginTop': 15}
+                    style={"font-size": 16, "text-align": 'left', 'marginTop': 15},
                 ),
             ]
         )
@@ -92,7 +98,7 @@ app.layout = dbc.Container([
                     )
                 ],
                 width={"size": 4},
-                className="my-3",
+                className="my-3", align = "center"
             ),
             dbc.Col(
                 [
@@ -115,6 +121,12 @@ app.layout = dbc.Container([
         dbc.Col([
             html.Div([
                 html.H3("Word Clouds by Stance"),
+                html.P(
+                "The word cloud provides a visual representation of the most salient "
+                "and frequently used terms related to climate change on Twitter over time. " 
+                "This can be a useful tool for understanding public discourse around climate "
+                "change and identifying trends and shifts in language and rhetoric."
+                ),
 
                 dcc.Dropdown(
                     options=[
@@ -124,13 +136,8 @@ app.layout = dbc.Container([
                     ],
                     value='Words',
                     id='wordcloud-dropdown',
-                    style={'width': '100%'}
+                    style={'width': '40%'}
                 ),
-                html.P("""
-                The word cloud provides a visual representation of the most salient and frequently used terms related to climate change on Twitter over time. 
-                This can be a useful tool for understanding public discourse around climate change and identifying trends and shifts in language and rhetoric. 
-                """)
-
             ]),
 
         ], width=12),
@@ -154,11 +161,37 @@ app.layout = dbc.Container([
         ], width=6),
     ]),
     dbc.Row(
+            dbc.Col(
+                html.H3("Average Sentiment of Tweets Before & After a Disaster Declaration"),
+                width={"size": 12},
+            )
+        ),
+        dbc.Row(
+            dbc.Col(
+                dcc.Graph(
+                    id="sentiment-plot", 
+                    figure=px.scatter(
+                        df2, 
+                        x="Year_Month", 
+                        y=["Avg_Sentiment_Before", "Avg_Sentiment_After"], 
+                        color_discrete_sequence=["blue", "red"], 
+                        hover_data=['declarationTitle']
+                    ).update_layout(
+                        title="Average Sentiment of Tweets Before & After a Disaster Declaration", 
+                        xaxis_title="Declaration Date", 
+                        yaxis_title="Average Sentiment"
+                    )
+                ),
+                width={"size": 12},
+            )),
+
+    dbc.Row(
         dbc.Col(
             [
                 html.P(
-                    "This project was developed by Jonathan Juarez, Jaskirat Kaur, Ridhi Purohit, and Grey Xu"
-                    "as part of the Spring 2023 course 'CAPP 30122: Computer Science and Applications II' at the University of Chicago",
+                    "This project was developed by Jonathan Juarez, Jaskirat Kaur, "
+                    "Ridhi Purohit, and Grey Xu as part of the Spring 2023 course "
+                    "'CAPP 30122: Computer Science and Applications II' at the University of Chicago",
                     className="text-muted",
                 ),
             ],
@@ -169,7 +202,8 @@ app.layout = dbc.Container([
 ], fluid=True)
 
 # ********************************** App callbacks *****************************
-# Callback and function for displaying the map
+
+# Callback for displaying the map
 @app.callback(
     Output('map-graph', 'figure'),
     [Input('stance_dropdown', 'value'),
@@ -202,11 +236,11 @@ def display_wordclouds_2009(stance):
     
     """
     if stance == "believer":
-        image_path = "30122-project-hot-or-not/main/images/wordcloud_believer_2009.png"
+        image_path = "./main/images/new_wordcloud_believer_2009.png"
     elif stance == "denier":
-        image_path = "30122-project-hot-or-not/main/images/wordcloud_denier_2009.png"
+        image_path = "./main/images/new_wordcloud_denier_2009.png"
     else:
-        image_path = "30122-project-hot-or-not/main/images/wordcloud_neutral_2009.png"
+        image_path = "./main/images/new_wordcloud_neutral_2009.png"
        
     encoded_image = base64.b64encode(open(image_path, 'rb').read())
 
@@ -223,15 +257,15 @@ def display_wordclouds_2019(stance):
     
     """
     if stance == "believer":
-        image_path = "30122-project-hot-or-not/main/images/wordcloud_believer_2019.png"
+        image_path = "./main/images/new_wordcloud_believer_2018.png"
     elif stance == "denier":
-        image_path = "30122-project-hot-or-not/main/images/wordcloud_denier_2019.png"
+        image_path = "./main/images/new_wordcloud_denier_2018.png"
     else:
-        image_path = "30122-project-hot-or-not/main/images/wordcloud_neutral_2019.png"
+        image_path = "./main/images/new_wordcloud_neutral_2018.png"
 
     encoded_image = base64.b64encode(open(image_path, 'rb').read())
 
     return 'data:image/png;base64,{}'.format(encoded_image.decode())
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, port=2505)
