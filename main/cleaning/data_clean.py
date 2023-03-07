ec5a1c8c4033d51a48cd56f0a598ec4ad8f9838c
grey-patrick-xu <114790396+grey-patrick-xu@users.noreply.github.com>
2023-03-06 22:03:55 -0600

    Update data_clean.py

--- main/cleaning/data_clean.py
+++ main/cleaning/data_clean.py
@@ -1,18 +1,27 @@
-from google.colab import drive
+"""
+Data Cleaning and API File
+
+Grey: Part 1, 3 and 5
+Jonathan: Part 2
+Jaskirat: Part 4
+
+"""
+import json
+from pandas import json_normalize
+import requests
+import re
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import datetime
-!pip install geopandas
+import us.states
 import geopandas as gpd
 from shapely.geometry import Point
 
-drive.mount('/content/drive')
-mydrive = "/content/drive/MyDrive/CS2_Project"
-!ls $mydrive  #double check you have the correct path by listing the contents of that folder
 
 ### Part 1. Twitter Dataset Clean 
-twitter = pd.read_csv("/content/drive/MyDrive/CS2_Project/twitter_archive/twitter.csv")
+twitter = pd.read_csv("./main/sources/twitter.csv")
+# You need to find this file in the google drive link provided in the readme. 
 twitter_log_lat = twitter[twitter.lng.notnull() & twitter.lat.notnull()]
 US_range = (twitter_log_lat.lng >= -124.848974) & (twitter_log_lat.lng <= -66.885444) & (twitter_log_lat.lat > 24.396308) & (twitter_log_lat.lat < 49.384358)
 US_twitter = twitter_log_lat[US_range]
@@ -20,8 +29,15 @@ US_twitter['time'] = pd.to_datetime(US_twitter['created_at'])
 US_twitter['date'] = US_twitter['time'].dt.date
 US_twitter['year'] = US_twitter['time'].dt.year
 
+file_path = './main/sources/US_twitter.csv'
+US_twitter.to_csv(file_path, index=False) 
+
 # Load data into a Pandas DataFrame
-file_path = "/content/drive/MyDrive/CS2_Project/us_county/cb_2018_us_county_500k.shp"
+<<<<<<< HEAD
+file_path = "./main/sources/county_shape/cb_2018_us_county_500k.shp"
+=======
+file_path = "./main/sources/county/cb_2018_us_county_500k.shp"
+>>>>>>> 06b37e5e0d9291f955648d4ec73510df3aeb4bde
 # Convert latitude and longitude values to Point objects
 geometry = [Point(xy) for xy in zip(US_twitter['lng'], US_twitter['lat'])]
 # Create a GeoDataFrame from the DataFrame and Point objects
@@ -34,19 +50,10 @@ result = gpd.sjoin(gdf, us_map, how='left', op='within')
 US_twitter['state_FIP'] = result['STATEFP']
 US_twitter['county_FIP'] = result['COUNTYFP']
 
+fips_to_state = {state.fips: state.name for state in us.states.STATES}
+US_twitter['state_name'] = US_twitter["state_FIP"].map(fips_to_state)
+
 ### Part 2. FEMA Dataset
-import pandas as pd
-import json
-from pandas import json_normalize
-import requests
-import re
-
-#attributes for data reference
-#https://www.fema.gov/openfema-data-page/disaster-declarations-summaries-v2
-
-#how disasters are declared
-#https://www.fema.gov/disaster/how-declared
-
 # Set the API endpoint and parameters
 url = "https://www.fema.gov/api/open/v2/DisasterDeclarationsSummaries"
 params = {
@@ -64,7 +71,7 @@ disaster_data = response.json()
 df_disasters = json_normalize(disaster_data["DisasterDeclarationsSummaries"])
 
 columns_to_keep = ["disasterNumber", "state", "declarationType",\
-                   "declarationDate", "incidentBeginDate", \
+                   "declarationDate", "incidentBeginDate","incidentEndDate", \
                    "fyDeclared", "incidentType", "declarationTitle",\
                    "fipsStateCode",	"fipsCountyCode"]
 
@@ -72,23 +79,15 @@ columns_to_keep = ["disasterNumber", "state", "declarationType",\
 regex = re.compile(r"T.*")
 df_disasters['declarationDate'] = df_disasters['declarationDate'].str.replace(regex, '')
 df_disasters["incidentBeginDate"] = df_disasters["incidentBeginDate"].str.replace(regex, '')
-
-#limiting to major disasters only
-#df_major_disasters = df_disasters.loc[df_disasters["declarationType"] == "DR"]
-
-#unique disaster types
-print(df_disasters.incidentType.unique())
-print(df_disasters.fyDeclared.unique())
-print(df_disasters.declarationType.unique())
-#sort by earliest date
-#df_major_disasters.sort_values(by='declarationDate', ascending = True, inplace = True) 
+df_disasters["incidentEndDate"] = df_disasters["incidentEndDate"].str.replace(regex, '')
 df_disasters = df_disasters[columns_to_keep]
 
+file_path = './main/sources/FEMA_disaster.csv'
+df_disasters.to_csv(file_path, index=False) 
 
 ### Part 3. Merge the twitter data with FEMA
-## Part 3.1. Merge based on both the event data and the county
+## Part 3.1. Merge based on both the event date and the county
 
-# Take 7 minute to run this line
 # Group data by date and county
 grouped_df = US_twitter.groupby(['date', 'county_FIP'])
 
@@ -108,14 +107,45 @@ result_df = grouped_df.agg(agg_dict)
 result_df = result_df.reset_index()
 
 # If the twitter has the same county ID with the FEMA data, and the twitter release date falls into the 
-# range of starting date and ending date of the event
+# range of starting date and ending date of the eventll
 df_disasters['declarationDate'] = pd.to_datetime(df_disasters['declarationDate']).dt.date
 df_disasters['incidentBeginDate'] = pd.to_datetime(df_disasters['incidentBeginDate']).dt.date
 df_disasters['incidentEndDate'] = pd.to_datetime(df_disasters['incidentEndDate']).dt.date
 merged_df = pd.merge(df_disasters, result_df, left_on='fipsCountyCode', right_on = 'county_FIP')
 filtered_df = merged_df[(merged_df['date'] >= merged_df['incidentBeginDate']) & (merged_df['date'] <= merged_df['incidentEndDate'])]
 
-## Part 3.2. Merge based on both the event data and the state
+## Part 3.1. Merge based on both the event date and county
+# Group data by date and county
+grouped_df = US_twitter.groupby(['date', 'county_FIP'])
+
+# Define the aggregation functions for categorical and numerical variables
+# For categorical value, take the most frequently occured type, for the numerical value, take the average
+agg_dict = {'topic': lambda x: x.value_counts().index[0], 
+            'stance': lambda x: x.value_counts().index[0], 
+            'gender': lambda x: x.value_counts().index[0], 
+            'aggressiveness': lambda x: x.value_counts().index[0], 
+            'state_name': lambda x: x.iloc[0],
+            'county_name': lambda x: x.iloc[0],
+            'temperature_avg': 'mean',
+            'sentiment': 'mean',
+            'id': lambda x: list(x)}
+
+# Apply the aggregation
+result_df = grouped_df.agg(agg_dict)
+# Reset the index
+result_df = result_df.reset_index()
+
+df_disasters['declarationDate'] = pd.to_datetime(df_disasters['declarationDate']).dt.date
+df_disasters['incidentBeginDate'] = pd.to_datetime(df_disasters['incidentBeginDate']).dt.date
+df_disasters['incidentEndDate'] = pd.to_datetime(df_disasters['incidentEndDate']).dt.date
+merged_df = pd.merge(df_disasters, result_df, left_on='fipsCountyCode', right_on = 'county_FIP')
+filtered_df = merged_df[(merged_df['date'] >= merged_df['incidentBeginDate']) & (merged_df['date'] <= merged_df['incidentEndDate'])]
+
+# There is only five events that are matched.
+file_path = './main/sources/merged_date_county.csv'
+filtered_df.to_csv(file_path, index=False) 
+
+## Part 3.2. Merge based on both the event date and the state
 # Take 3 minute to run this line
 # Group data by date and county
 grouped_df2 = US_twitter.groupby(['date', 'state_FIP'])
@@ -126,45 +156,61 @@ agg_dict2 = {'topic': lambda x: x.value_counts().index[0],
             'stance': lambda x: x.value_counts().index[0], 
             'gender': lambda x: x.value_counts().index[0], 
             'aggressiveness': lambda x: x.value_counts().index[0], 
+            'state_name': lambda x: x.iloc[0],
             'temperature_avg': 'mean',
-            'sentiment': 'mean'}
+            'sentiment': 'mean',
+             'id': lambda x: list(x)}
 
 # Apply the aggregation
 result_df2 = grouped_df2.agg(agg_dict2)
-
 # Reset the index
 result_df2 = result_df2.reset_index()
 
 # If the twitter release date falls into the range of starting date and ending date of the event,
 # and we don't consider whether the twitter has the same county ID with the FEMA data 
-df_disasters['declarationDate'] = pd.to_datetime(df_disasters['declarationDate']).dt.date
-df_disasters['incidentBeginDate'] = pd.to_datetime(df_disasters['incidentBeginDate']).dt.date
-df_disasters['incidentEndDate'] = pd.to_datetime(df_disasters['incidentEndDate']).dt.date
 merged_df2 = pd.merge(df_disasters, result_df2, left_on='fipsStateCode', right_on = 'state_FIP')
 filtered_df2 = merged_df2[(merged_df2['date'] >= merged_df2['incidentBeginDate']) & (merged_df2['date'] <= merged_df2['incidentEndDate'])]
 
+file_path = './main/sources/merged_date_state.csv'
+filtered_df2.to_csv(file_path, index=False) 
 
-###Part 4. Getting Twitter API for text data
-!pip install twarc
-import twarc
+# Part 4. Create believer, netural and denier twitter csv for dashboard
+def stance_data_generator(stance):
+    """
+    This function generates data files filtered by stance out of the large US_Twitter dataset 
+    Input: stance(Believer, Denier or Neutral)
+    Return: None, created the file in the data directory
+    """
+    stance_US = US_twitter.drop(["created_at","id",	"lng", 'lat', "topic","gender", "temperature_avg","aggressiveness", 
+                "time",	"date",	"geometry", "county_FIP", 'county_name'], axis=1)
+    stance_US = stance_US.dropna()
+    stance_US = stance_US[(stance_US["state_FIP"] != 2006) & (stance_US["state_FIP"] != 2007) & (stance_US["state_FIP"] != 2008)]
+    stance_US = stance_US[(stance_US["stance"] == stance)]
+    file_path = f'./main/sources/{stance}_twitter.csv'
+    stance_US.to_csv(file_path, index = False)
 
-# Set your Twitter API credentials
-consumer_key = "yCxPJWffCtcCQdqNb6ZGShglm"
-consumer_secret = "ijqgx8YSaxsVG0TAQbcaQSeSTGhSMxW4VH8aQewO0H1nJxxLX6"
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAN3xlQEAAAAAkV9D6etUpGV1y3rnrZoPgAhSC7w%3DKmU8RZJ9REiwP4kvS4XMryEfRq6rmPta04yQIMBBUXoUEE36wP"
+stance_data_generator("believer")
+stance_data_generator("denier")
+stance_data_generator("neutral")
 
-# Initialize a Twarc instance with your API credentials, activate the below code if you are ready to pull.
-t = twarc.Twarc(consumer_key, consumer_secret, bearer_token=bearer_token)
+# Part 5. Merging the tweets data county level with tweet text, pulled from tweet API
+## Use to generate the word cloud, tweets_text_content.csv is created from pulling the data using twarc
+tweets_text = pd.read_csv("./main/sources/tweets_text_content.csv")
+tweets_text['content'] = tweets_text.apply(lambda x: (x['text'] if type(x['text']) == str else '') +
+                        (x['Unnamed: 2'] if type(x['Unnamed: 2']) == str else '') + (x['Unnamed: 3'] if type(x['Unnamed: 3']) == str else '') +
+                        (x['Unnamed: 4'] if type(x['Unnamed: 4']) == str else '') + (x['Unnamed: 5'] if type(x['Unnamed: 5']) == str else '') +
+                        (x['Unnamed: 6'] if type(x['Unnamed: 6']) == str else '') + (x['Unnamed: 7'] if type(x['Unnamed: 7']) == str else '') +
+                        (x['Unnamed: 8'] if type(x['Unnamed: 8']) == str else '') + (x['Unnamed: 9'] if type(x['Unnamed: 9']) == str else '') +
+                        (x['Unnamed: 10'] if type(x['Unnamed: 10']) == str else '') + (x['Unnamed: 11'] if type(x['Unnamed: 12']) == str else '') +
+                        (x['Unnamed: 12'] if type(x['Unnamed: 12']) == str else '') + (x['Unnamed: 13'] if type(x['Unnamed: 13']) == str else '') +
+                        (x['Unnamed: 14'] if type(x['Unnamed: 14']) == str else ''), axis=1)
 
-# Set the tweet ID(s) you want to hydrate
-### IMPORTANT!!!!###
-# Because we only have 500k of tweet we could use, we have to subset based on the data we have
-# i.e. US_twitter_2019 has 383k of data
-tweet_ids = [] # this could be access by asking US_twitter_2019['id']
+tweets_text['id'] = tweets_text['id'].astype(str).str.slice(stop=-1)
+tweets_text_final = tweets_text.loc[:,["id","content"]].drop_duplicates()
+filter_exploded = filtered_df.explode('id')
+filter_exploded['id'] = filter_exploded['id'].astype(str)
+resulting_text_file = filter_exploded.merge(tweets_text_final, on='id', how = 'inner').drop_duplicates()
 
-# Hydrate the tweets and write the hydrated data to a file
-with open("tweets.jsonl", "w") as f:
-    for tweet in t.hydrate(tweet_ids):
-        f.write(tweet)
-        f.write("\n")
+file_path = './main/sources/merged_twitter_text_county.csv'
+resulting_text_file.to_csv(file_path, index=False)
 
