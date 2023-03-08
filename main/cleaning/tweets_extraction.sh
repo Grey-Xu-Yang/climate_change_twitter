47ee60fe9c8eb3bf413844f4922d5034436f2832
ridhi96 <ridhi@uchicago.edu>
2023-03-07 19:22:31 -0600

    Delete tweets_extraction.sh

--- main/cleaning/tweets_extraction.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# code contribution - Ridhi
-# This shell script hydrates tweets using twarc library and a set of unique tweet ids
-pip install --upgrade twarc
-twarc2 configure
-twarc2 hydrate ./tweet_ids_random.txt hydrated_tweets.json
